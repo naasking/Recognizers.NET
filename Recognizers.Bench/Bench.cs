20a622dd72bc3ec761e28a2b7ce29ea4d919ca43
Sandro Magi <none@none>
2020-04-15 16:38:26 -0400

    added a simple regex benchmark

---
+++ Recognizers.Bench/Bench.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Recognizers;
+
+namespace Recognizers.Bench
+{
+    //[SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+    //[SimpleJob(RuntimeMoniker.NetCoreApp30)]
+    [RPlotExporter]
+    public class Bench
+    {
+        const string regex = "[\x020]*[+]?[\x020]*[0-9]*[\x020]*([(][0-9]+[)])?[\x020]*[/]?([0-9-\x020])+";
+        static string[] phonenos = new[]
+        {
+            "555-555",
+            "(555) 555-555",
+            "555 555-555",
+            "555-555-555",
+            "555-555 555",
+            "+1 555-555 555",
+            "+1 (555) 555 555",
+            "(089) / 636-48018",
+            "+49-89-636-48018",
+            "19-49-89-636-48018",
+            "191 541 754 3010",
+        };
+
+        [Benchmark]
+        public void BenchRecognizers()
+        {
+            for (int i = 0; i < phonenos.Length; ++i)
+            {
+                var pos = new Position();
+                var input = new Input(phonenos[i]);
+                input.PhoneNumber(ref pos);
+            }
+        }
+
+        [Benchmark]
+        public void BenchRegex()
+        {
+            var rx = new System.Text.RegularExpressions.Regex(regex);
+            for (int i = 0; i < phonenos.Length; ++i)
+            {
+                var input = phonenos[i];
+                var match = rx.Match(input);
+            }
+        }
+
+        [Benchmark]
+        public void BenchRegexCompiled()
+        {
+            var rx = new System.Text.RegularExpressions.Regex(regex, System.Text.RegularExpressions.RegexOptions.Compiled);
+            for (int i = 0; i < phonenos.Length; ++i)
+            {
+                var input = phonenos[i];
+                var match = rx.Match(input);
+            }
+        }
+    }
+}
---
+++ Recognizers.Bench/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Recognizers.Bench
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ManualConfig.CreateEmpty()
+            //    .AddJob(Job.Default.WithJit(BenchmarkDotNet.Environments.Jit.RyuJit)
+            BenchmarkRunner.Run<Bench>();
+        }
+    }
+}
